23a1bcda6c01fd02c95b75c5e700fe80511355f0
Hardwell <ac4gaming.1220@gmail.com>
2025-12-24 22:25:33 +0900

    sw

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-6d1fb45c'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.l54g3tcsg08"
+    "revision": "0.6noicnnkrq"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
